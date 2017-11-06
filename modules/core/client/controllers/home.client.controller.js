7cfe64b384052005b3c304fac0a062ed9a80da7f
Chris Landry <chris.landry@onlinebankingsolutions.com>
2017-11-06 18:30:22 -0500

    name search returns json records from fhir server

--- modules/core/client/controllers/home.client.controller.js
+++ modules/core/client/controllers/home.client.controller.js
@@ -5,12 +5,16 @@
     .module('core')
     .controller('HomeController', HomeController);
 
-  function HomeController() {
+  function HomeController($http) {
     var vm = this;
     vm.patientName = "";
 
     vm.patientSearch = function() {
-      alert(vm.patientName + " not found because we have no API connection!");
+      $http.get('https://fhirtesting.hdap.gatech.edu/hapi-fhir-jpaserver-example/baseDstu3/Patient?name='+vm.patientName).success(function(data) {
+        alert(JSON.stringify(data));
+      }).error(function(data) {
+        alert("error!" + JSON.stringify(data));
+      });
     }
   }
 }());
